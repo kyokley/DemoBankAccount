5a7ec93a368a454799174593e86f8cc02bf073ef
Kevin Yokley <kyokley2@gmail.com>
2017-11-19 12:41:05 -0600

    Adding test for preferred

--- test_bank_account.py
+++ test_bank_account.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 from bank_account import BankAccount
 
@@ -12,8 +13,19 @@ class TestDeposit(unittest.TestCase):
         expected = 100
         actual = self.account.get_balance()
         self.assertEqual(expected, actual)
+        self.assertFalse(self.account.preferred)
 
     def test_negative_deposit(self):
         self.assertRaises(Exception,
                           self.account.deposit,
                           -100)
+
+    @mock.patch('bank_account.send_email', autospec=True)
+    def test_preferred(self, mock_send_email):
+        self.account.deposit(1000)
+
+        expected = 1000
+        actual = self.account.get_balance()
+        self.assertEqual(expected, actual)
+        mock_send_email.assert_called_once_with("Congrats! You've qualified for preferred status!")
+        self.assertTrue(self.account.preferred)
--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import time
 
 def send_email(msg):
-    # This is a fake function to simulate needing to do a lot of work
-    # We'll just sleep for a bit and pretend things are happening...
-    time.sleep(10)
-    print('Mail has been sent!')
+    # This is a fake function to be mocked in tests
+    assert False
